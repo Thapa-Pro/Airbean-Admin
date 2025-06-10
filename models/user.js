562dbe754da661daefd82f9c893abf3b8cc20c70
Thapa-Pro <bachchan.k.t@gmail.com>
2025-06-10 10:54:29 +0200

    menu related

--- models/user.js
+++ models/user.js
@@ -1,20 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user'
-  }
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }, //enumeration is used to restrict a field to a specific set of values.
 });
 
 module.exports = mongoose.model('User', userSchema);
